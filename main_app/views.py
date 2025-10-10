b03803f5d17c76f317e8fe4ed557fe42b9f07023
KieshaWalker <llcwalkerk@gmail.com>
2025-10-10 14:30:33 -0500

    full crud completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,52 @@
 # main_app/views.py
 
 from django.shortcuts import render
+from django.contrib import admin
+from django.views.generic.edit import CreateView
+# Add UdpateView & DeleteView
+from django.views.generic.edit import UpdateView, DeleteView
+
+from .models import Cat
+admin.site.register(Cat)
+
 
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
+
+
 # Define the home view function
+
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'base.html')
+
+def homeTwo(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
 
 # main_app/about
 
 def about(request):
     return render(request, 'about.html')
 
+# views.py
+
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
---
+++ readme.md
@@ -0,0 +1,6 @@
+As a User, when I click the All Cats link, I want to see a page listing all of my cats so that I can see them all one one place.
+
+For this User story, we need to build an index page to render a list of all our our cats. To make the UI of this page more appealing , we’ll list our cats using “card” style elements in the UI. Each card will have an image placeholder and some brief details about each cat.
+
+Here is a wireframe of the final design:
+![wireframe](image.png)
